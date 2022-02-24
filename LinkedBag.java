0fad72e871c80fbc2705542e207ccf7457de092f
George Matta <jojomedhat2004@gmail.com>
2022-02-24 15:13:33 -0800

    Removed unnecessary commit

--- LinkedBag.java
+++ LinkedBag.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Currency;
 
 class Node<T> {
     T data;
@@ -59,7 +58,7 @@ public class LinkedBag<T> implements BagInterface<T> {
 
     /**
      * Finds a given entry within this bag.
-     * 
+     *
      * @param anEntry The entry to search
      * @return A reference to the node, otherwise null
      */
