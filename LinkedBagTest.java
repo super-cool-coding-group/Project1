4dbc86bd4acc799efe3096d2245cce769b168ff8
angieArt23 <angelicaa2@cpp.edu>
2022-02-23 16:33:17 -0800

    changes to LinkedBagTest

--- LinkedBagTest.java
+++ LinkedBagTest.java
@@ -2,7 +2,6 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-
 import org.junit.Test;
 
 public class LinkedBagTest {
@@ -10,20 +9,20 @@ public class LinkedBagTest {
     BagInterface<Character> testBag = new LinkedBag<Character>();
     BagInterface<Character> testBag2 = new LinkedBag<Character>();
 
-    @Test
+    @Test //Done
     public void testAdd() {
         testBag.add('a');
         assertTrue(testBag.contains('a'));
     }
 
-    @Test
+    @Test //Done
     public void testContains() {
         testBag.add('a');
         assertTrue(testBag.contains('a'));
         assertFalse(testBag.contains('d'));
     }
 
-    @Test
+    @Test //Done
     public void testToArray() {
         testBag.add('c');
         testBag.add('b');
@@ -32,7 +31,7 @@ public class LinkedBagTest {
         assertArrayEquals(expected, testBag.toArray());
     }
 
-    @Test
+    @Test //Done
     public void testClear() {
         testBag.add('a');
         testBag.add('b');
@@ -42,29 +41,8 @@ public class LinkedBagTest {
         int expected2 = 0;
         assertEquals(expected2, testBag.getCurrentSize());
     }
-    
-    //fix difference test
-    @Test
-    public void testDifference() {
-        testBag.add('a');
-        testBag.add('b');
-        testBag.add('c');
-        testBag2.add('c');
-        testBag2.add('d');
-        testBag2.add('e');
-        BagInterface<Character> outcome = testBag.difference(testBag2);
-        System.out.println(testBag.difference(testBag2));
-        assertTrue(outcome.contains('a'));
-        assertTrue(outcome.contains('b'));
-        assertFalse(outcome.contains('c'));
-        assertFalse(outcome.contains('d'));
-        assertFalse(outcome.contains('e'));
 
-
-
-    }
-
-    @Test
+    @Test //Done
     public void testGetCurrentSize() {
         testBag.add('a');
         testBag.add('b');
@@ -72,63 +50,84 @@ public class LinkedBagTest {
         assertEquals(expected, testBag.getCurrentSize());
     }
 
-    @Test
+    @Test //Done
     public void testGetFrequencyOf() {
         testBag.add('a');
         testBag.add('a');
         testBag.add('b');
         int expected = 2;
         assertEquals(expected, testBag.getFrequencyOf('a'));
+        int expected2 = 1;
+        assertEquals(expected2, testBag.getFrequencyOf('b'));
     }
 
-    @Test
-    public void testIntersection() {
-        testBag.add('a');
-        testBag.add('b');
-        testBag.add('c');
-        testBag2.add('b');
-        testBag2.add('c');
-        testBag2.add('d');
-        System.out.println(testBag.intersection(testBag2));
-    }
-
-    @Test
+    @Test //Done
     public void testIsEmpty() {
         assertTrue(testBag.isEmpty());
         testBag.add('a');
         assertFalse(testBag.isEmpty());
     }
 
-    @Test
+    @Test //Done
     public void testRemove() {
         testBag.add('a');
+        assertTrue(testBag.contains('a')); 
         testBag.remove('a');
         assertFalse(testBag.contains('a')); 
     }
 
-    @Test
+    @Test //Work on remove 2
     public void testRemove2() {
         
     }
-    // fix toString test
-    @Test
+    @Test //Fix toString test
     public void testToString() {
         testBag.add('a');
         testBag.add('b');
         testBag.add('c');
         System.out.println(testBag.toString());
-        
     }
-    //fix union test
-    @Test
+    
+    @Test //Done
     public void testUnion() {
         testBag.add('a');
         testBag.add('b');
-        testBag.add('c');
         testBag2.add('c');
         testBag2.add('d');
-        testBag2.add('e');
-        System.out.println(testBag.union(testBag2)); 
+        BagInterface<Character> outcome = testBag.union(testBag2);
+        assertTrue(outcome.contains('a'));
+        assertTrue(outcome.contains('b'));
+        assertTrue(outcome.contains('c'));
+        assertTrue(outcome.contains('d'));
+        //System.out.println(testBag.union(testBag2)); 
+    }
+
+    @Test //Done
+    public void testDifference() {
+        testBag.add('a');
+        testBag.add('b');
+        testBag2.add('b');
+        testBag2.add('c');
+        BagInterface<Character> outcome = testBag.difference(testBag2);
+        BagInterface<Character> outcome2 = testBag2.difference(testBag);
+        assertTrue(outcome.contains('a'));
+        assertTrue(outcome2.contains('c'));
+        assertFalse(outcome.contains('b'));
+        assertFalse(outcome2.contains('b'));
+        //System.out.println(testBag.difference(testBag2));
+        //System.out.println(testBag2.difference(testBag));
+    }
+
+    @Test //Done
+    public void testIntersection() {
+        testBag.add('a');
+        testBag.add('b');
+        testBag2.add('b');
+        testBag2.add('c');
+        BagInterface<Character> outcome = testBag.intersection(testBag2);
+        assertTrue(outcome.contains('b'));
+        assertFalse(outcome.contains('a'));
+        //System.out.println(testBag.intersection(testBag2));
     }
 
 }
