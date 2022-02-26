47abad0c422801d56f1f41753d3dc713227e97eb
angieArt23 <angelicaa2@cpp.edu>
2022-02-26 01:10:11 -0800

    updates to ArrayBagTest.java

--- ArrayBagTest.java
+++ ArrayBagTest.java
@@ -1,7 +1,7 @@
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
@@ -17,6 +17,121 @@ public class ArrayBagTest{
 
     BagInterface<Character> testBag = new ResizeableArrayBag<Character>();
     BagInterface<Character> testBag2 = new ResizeableArrayBag<Character>();
+
+    @Test
+    public void testAdd(){
+        testBag.add('a');
+        assertTrue(testBag.contains('a'));
+    }
+
+    @Test
+    public void testSetBag(){
+        Character[] testArray = {'d', 'e', 'f'};
+        ((ResizeableArrayBag<Character>) testBag).setBag(testArray);
+    }
+    
+    @Test
+    public void testGetBag(){
+
+    }
+
+    @Test
+    public void testGetDefaultCapacity(){
+        int expected = 10;
+        assertEquals(expected, testBag.getDefaultCapacity());
+    }
+
+    @Test
+    public void testGetCapacity(){
+        int expected;
+        assertEquals(expected, testBag.getCapacity());
+    }
+
+    @Test
+    public void testGetNumOfEntries(){
+        testBag.add('a');
+        testBag.add('b');
+        testBag.add('c');
+        int expected = 3;
+        assertEquals(expected, testBag.getNumOfEntries());
+    }
+
+    @Test
+    public void testGetMaxEntries(){
+
+    }
+
+    @Test
+    public void testCalculateNumOfEntries(){
+        testBag.add('a');
+        testBag.add('b');
+        testBag.add('c');
+        int expected = 3;
+        assertEquals(expected, testBag.calculateNumOfEntries());
+    }
+
+    @Test // A method to remove the last item from a ResizeableArrayBag's bag
+    public void testRemove(){
+        testBag.add('a');
+        testBag.add('b');
+        testBag.remove();
+        assertFalse(testBag.contains('b')); 
+        assertTrue(testBag.contains('a'));
+    }
+
+    @Test // A method to remove an item from a ResizeableArrayBag's bag given an index
+    public void testRemove2(){
+        testBag.add('a');
+        testBag.add('b');
+        testBag.remove(1);
+        assertFalse(testBag.contains('b'));
+        assertTrue(testBag.contains('a'));
+    }
+
+    @Test // A method to remove a specified item from a ResizeableArrayBag's bag
+    public void testRemove3(){
+        testBag.add('a');
+        testBag.add('b');
+        testBag.remove('b');
+        assertFalse(testBag.contains('b'));
+        assertTrue(testBag.contains('a'));
+    }
+
+    @Test
+    public void testGetIndexOf(){
+        testBag.add('a');
+        testBag.add('b');
+        testBag.add('b');
+        int expected = 1;
+        assertEquals(testBag.getIndexOf('b'));
+    }
+
+    @Test
+    public void testResizeBag(){
+
+    }
+
+    @Test
+    public void testToArray(){
+        testBag.add('a');
+        testBag.add('b');
+        testBag.add('c');
+        Character[] expected = {'a', 'b', 'c'};
+        assertArrayEquals(expected, testBag.toArray());
+    }
+
+    @Test
+    public void testToSortedArrayList(){
+
+    }
+
+    @Test 
+    public void testToString(){
+        testBag.add('a');
+        testBag.add('b');
+        String expected = "[a, b]"; 
+        assertEquals(expected, testBag.toString());
+    }
 
     @Test
     public void testUnion(){
@@ -59,96 +174,4 @@ public class ArrayBagTest{
         assertFalse(outcome.contains('a'));
         //System.out.println(testBag.intersection(testBag2));
     }
-
-    @Test
-    public void testSetBag(){
-
-    }
-    
-    @Test
-    public void testGetBag(){
-
-    }
-
-    @Test
-    public void testGetDefaultCapacity(){
-
-    }
-
-    @Test
-    public void testCapacity(){
-
-    }
-
-    @Test
-    public void testGetNumOfEntries(){
-
-    }
-
-    @Test
-    public void testGetMaxEntries(){
-
-    }
-
-    @Test
-    public void testCalculateNumOfEntries(){
-
-    }
-
-    @Test
-    public void testAdd(){
-        testBag.add('a');
-        assertTrue(testBag.contains('a'));
-    }
-
-    @Test
-    public void testRemove(){
-        testBag.add('a');
-        assertTrue(testBag.contains('a')); 
-        testBag.remove('a');
-        assertFalse(testBag.contains('a')); 
-    }
-
-    @Test
-    public void testRemove2(){
-        testBag.add('a');
-        testBag.add('b');
-        testBag.remove();
-        assertFalse(testBag.contains('a'));
-    }
-
-    @Test
-    public void testRemove3(){
-        
-    }
-
-    @Test
-    public void testGetIndexOf(){
-
-    }
-
-    @Test
-    public void testResizeBag(){
-
-    }
-
-    @Test
-    public void testToArray(){
-        testBag.add('c');
-        testBag.add('b');
-        testBag.add('a');
-        Character[] expected = {'a', 'b', 'c'};
-        assertArrayEquals(expected, testBag.toArray());
-    }
-
-    @Test
-    public void testToSortedArrayList(){
-
-    }
-
-    @Test
-    public void testToString(){
-
-    }
-
 }
