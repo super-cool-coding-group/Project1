710f5910cac5de9b557f74cc9e9093bffa7c623f
pperuzzo <pierlorenzo.peruzzo@gmail.com>
2022-02-27 19:05:26 -0800

    Fix driver code

--- bagDriver.java
+++ bagDriver.java
@@ -1,18 +1,25 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * 
+ * This is the bagDriver class. We use this class to demo some typical usage of union, intersection, and difference in BagInterface.
+ *
+ * @author George Matta
+ * @author Pierlorenzo Peruzzo
+ * @version 1.2
+ */
 public class bagDriver {
 
     public static void main(String[] args) {
 
+        // ResizableArrayBag test
+        System.out.println("***Testing ResizableArrayBag***");
         String[] arrayOne = { "a", "b", "c" }; // The first list that we create a bag from
         String[] arrayTwo = { "b", "b", "d", "e" }; // The second list that we create a bag from
-
         ResizeableArrayBag<String> bagOne = new ResizeableArrayBag<String>(arrayOne); // Our first bag
         ResizeableArrayBag<String> bagTwo = new ResizeableArrayBag<String>(arrayTwo); // Our second bag
 
-
-        // Order is unimportant
         // Union
         System.out.println("Union");
         System.out.println(bagOne.union(bagTwo)); // a b b b c d e
@@ -28,14 +35,16 @@ public class bagDriver {
         System.out.println(bagOne.difference(bagTwo)); // a c
         System.out.println(bagTwo.difference(bagOne)); // b d e
 
-        ArrayList<String> listOne = new ArrayList<>(Arrays.asList("a", "b", "c")); // The first list that we create a
-                                                                                   // bag from
-        ArrayList<String> listTwo = new ArrayList<>(Arrays.asList("b", "b", "d", "e")); // The second list that we
-                                                                                        // create a bag from
+
+        // LinkedBag test
+        System.out.println("\n\n***Testing LinkedBag***");
+        ArrayList<String> listOne = new ArrayList<>(Arrays.asList("a", "b", "c")); // The first list that we create a bag from
+        ArrayList<String> listTwo = new ArrayList<>(Arrays.asList("b", "b", "d", "e")); // The second list that we create a bag from
         LinkedBag<String> linkedBagOne = new LinkedBag<String>(listOne); // Our first bag
         LinkedBag<String> linkedBagTwo = new LinkedBag<String>(listTwo); // Our second bag
 
-        System.out.println("\nUnion");
+        // Union
+        System.out.println("Union");
         System.out.println(linkedBagOne.union(linkedBagTwo)); // a b b b c d e
         System.out.println(linkedBagTwo.union(linkedBagOne)); // a b b b c d e
 
@@ -48,7 +57,5 @@ public class bagDriver {
         System.out.println("\nDifference");
         System.out.println(linkedBagOne.difference(linkedBagTwo)); // a c
         System.out.println(linkedBagTwo.difference(linkedBagOne)); // b d e
-
     }
-
 }
